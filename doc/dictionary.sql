87a2a4b496e45c3bdea7c4cb13ed454a657e1378
linqi <383118820@qq.com>
2019-06-13 23:42:48 +0800

    测试代码

---
+++ doc/dictionary.sql
@@ -0,0 +1,60 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.21-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('3','是否代码','D_SYS_SFDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('4','性别代码','D_SYS_XBDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('8','政治面貌','D_SYS_ZZMMDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('16','健康状况','D_SYS_JKZKDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('24','服务来源','D_SYS_FWLYDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('25','家庭类型','D_SYS_JTLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('26','人员类型','D_SYS_RYLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('27','自述需求','D_SYS_ZSXQDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('28','资源类型','D_SYS_ZYLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('30','结案类型','D_SYS_JALXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('31','服务对象结案反应','D_SYS_FWDXJAFYDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('32','结案反应处理','D_SYS_JAFYCLDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('33','督导接案意见','D_SYS_DDJAYJDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('34','小组类型','D_SYS_XZLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('35','组员招募方式','D_SYS_ZYZMFSDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('36','评估方法','D_SYS_PGFFDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('37','宣传方法','D_SYS_XCFFDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('38','督导方式','D_SYS_DDFSDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('40','人员角色代码','D_SYS_RYJSDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('41','执行区域','D_SYS_ZXQYDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('42','进度表类型','D_SYS_JDBLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('43','活动预告及信息发布','D_SYS_HDYGDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('44','图片所属类型','D_SYS_TPSSLXDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('45','重点关注群体','D_SYS_ZDGZQTDM','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('46','个人生活自理','D_SYS_GRSHZL','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('47','职业和工作','D_SYS_ZYHGZ','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('48','家庭职能','D_SYS_JTZN','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('49','婚姻职能','D_SYS_HYZN','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('50','父母职能','D_SYS_FMZN','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('51','家庭内活动情况','D_SYS_JTNHDQK','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('52','社会性退缩','D_SYS_SHXTS','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('53','家庭外的社会活动','D_SYS_JTWDSHHD','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('54','对外界的兴趣和关心','D_SYS_DWJDXQYGX','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('55','责任心和计划性','D_SYS_ZRXYJHX','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('56','基本健康状况','D_SYS_JBJKZK','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('57','自理能力','D_SYS_ZLNL','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('58','照护情况','D_SYS_ZHQK','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('59','疾病类型','D_SYS_JBLX','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('60','残疾类型','D_SYS_CJLX','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('61','案主是否精神病患','D_SYS_AZSFJSBH','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('62','家庭主要收入来源','D_SYS_JTZYSRLY','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('63','家庭主要支出类别','D_SYS_JTZYZCLB','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('64','住房性质','D_SYS_ZFXZ','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('65','一般服务','D_SYS_YBFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('66','资金帮扶','D_SYS_ZJBF','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('67','物质支持','D_SYS_WZZC','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('68','医疗援助服务','D_SYS_YLYZFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('69','康复支持服务','D_SYS_KFZCFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('70','住房援助服务','D_SYS_ZFYZFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('71','创就业援助服务','D_SYS_CJYYZFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('72','教育援助服务','D_SYS_JYYZFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('73','其他援助服务','D_SYS_QTYZFW','0','1');
+insert into `dictionary` (`id`, `name`, `eng_name`, `state`, `type`) values('74','户籍所在区','D_SYS_HJSZQDM','0','1');
